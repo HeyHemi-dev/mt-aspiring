d6e726c26f359707856de605c20e3e11b9227cf5
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-08-21 10:37:49 +1200

    Start defining routes ready for pages

--- client/index.tsx
+++ client/index.tsx
@@ -1,16 +1,29 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom'
 
+//Import pages
 import App from './components/App.tsx'
 
+//Define routes
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>),
+)
+
+//Define queryclient
 const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools />
-    </QueryClientProvider>
+    </QueryClientProvider>,
   )
 })
