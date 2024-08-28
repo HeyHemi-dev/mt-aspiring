497fc060e7319d150be8d7da47be18a1d0b060be
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-08-28 13:35:13 +1200

    Create simple tile ui

--- client/components/TileListItem.tsx
+++ client/components/TileListItem.tsx
@@ -1,11 +1,29 @@
 import { Tile } from 'model/tiles'
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
 
 function TileListItem({ tile }: { tile: Tile }) {
   return (
-    <>
-      <div>Tile</div>
-      <h2>{tile.title}</h2>
-    </>
+    <Card>
+      <div className="tile-item-layout grid grid-rows-[auto_1fr]">
+        <AspectRatio ratio={1 / 1} className="bg-muted">
+          <img
+            src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+            alt={tile.title}
+            className="h-full w-full rounded-md object-cover"
+          />
+        </AspectRatio>
+        <CardHeader>
+          <CardTitle>{tile.title}</CardTitle>
+          <CardDescription>{tile.description}</CardDescription>
+        </CardHeader>
+      </div>
+    </Card>
   )
 }
 
