c397ed846c09ec246b54852ad9d9b56aa66e7382
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-08-28 12:10:43 +1200

    Make tile look good

--- client/components/TileExpanded.tsx
+++ client/components/TileExpanded.tsx
@@ -1,14 +1,42 @@
 import { Tile } from 'model/tiles'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
+import { Button } from './ui/button'
 
 function TileExpanded({ tile }: { tile: Tile }) {
   return (
-    <div>
-      <h2>{tile.title}</h2>
-      <img src={tile.imagePath} alt={tile.title} />
-      <p>{tile.description}</p>
-      <p>Created by: {tile.createdBy}</p>
-      <p>Created at: {tile.createdAt?.toString()}</p>
-    </div>
+    <Card>
+      <div className="tile-expanded-layout grid grid-cols-2">
+        <div className="tile-expanded-image">
+          <AspectRatio ratio={1 / 1} className="bg-muted">
+            <img
+              src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+              alt={tile.title}
+              className="h-full w-full rounded-md object-cover"
+            />
+          </AspectRatio>
+        </div>
+        <div className="tile-expanded-content grid grid-rows-[auto_1fr_auto]">
+          <CardHeader>
+            <CardTitle>{tile.title}</CardTitle>
+            <CardDescription>{tile.description}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <h3>Featured Vendors</h3>
+          </CardContent>
+          <CardFooter className="flex flex-row-reverse">
+            <Button>Save</Button>
+          </CardFooter>
+        </div>
+      </div>
+    </Card>
   )
 }
 
