725b91542ff0394d35d767ca7fbb7470b9aa28d4
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-08-28 15:04:33 +1200

    Setup knex

---
+++ server/db/index.ts
@@ -0,0 +1,8 @@
+import knexFile from './knexfile.js'
+import knex from 'knex'
+
+type Environment = 'production' | 'test' | 'development'
+
+const environment = (process.env.NODE_ENV || 'development') as Environment
+const config = knexFile[environment]
+export const connection = knex(config)
