2c4f41392c7a78f1b25fec5f772436bb5c51963f
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-09-07 18:14:20 +1200

    Refactor database functions

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,8 +1,74 @@
 import knexFile from './knexfile.js'
 import knex from 'knex'
+import { SavedTileData, UserSavedTilesTable, TilesTable } from 'model/tiles.ts'
+import { UsersTable } from 'model/users.ts'
 
 type Environment = 'production' | 'test' | 'development'
 
 const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
 export const connection = knex(config)
+
+//Server Functions
+
+export function getAllPublicTiles() {
+  return connection('tiles').where(TilesTable.isPrivate, 0).select()
+}
+
+export function getTileById(tileId: number) {
+  return connection('tiles').where(TilesTable.id, tileId).select().first()
+}
+
+export function getUserSavedTiles(userId: number) {
+  return connection('users')
+    .where(`users.${UsersTable.id}`, userId)
+    .join(
+      'user_saved_tiles',
+      `user_saved_tiles.${UserSavedTilesTable.savedBy}`,
+      `users.${UsersTable.id}`,
+    )
+    .join(
+      'tiles',
+      `tiles.${TilesTable.id}`,
+      `user_saved_tiles.${UserSavedTilesTable.tileId}`,
+    )
+    .select('tiles.*')
+}
+
+export function getSavedTileRecord(recordId: number) {
+  return connection('user_saved_tiles')
+    .where(UserSavedTilesTable.id, recordId)
+    .select()
+    .first()
+}
+
+export function findSavedTileRecord(tileId: number, userId: number) {
+  return connection('user_saved_tiles')
+    .where(UserSavedTilesTable.savedBy, userId)
+    .where(UserSavedTilesTable.tileId, tileId)
+    .select()
+    .first()
+}
+
+export function updateSavedTileRecord(data: SavedTileData) {
+  return connection('user_saved_tiles')
+    .connection('user_saved_tiles')
+    .where(UserSavedTilesTable.savedBy, data.savedBy)
+    .where(UserSavedTilesTable.tileId, data.tileId)
+    .update({
+      [UserSavedTilesTable.isSaved]: data.isSaved,
+      [UserSavedTilesTable.updatedAt]: data.updatedAt,
+    })
+    .returning(UserSavedTilesTable.id)
+}
+
+export function createSavedTileRecord(data: SavedTileData) {
+  return connection('user_saved_tiles')
+    .insert({
+      [UserSavedTilesTable.savedBy]: data.savedBy,
+      [UserSavedTilesTable.tileId]: data.tileId,
+      [UserSavedTilesTable.isSaved]: data.isSaved,
+      [UserSavedTilesTable.updatedAt]: data.updatedAt,
+    })
+    .returning(UserSavedTilesTable.id)
+}
--- server/routes/tiles.ts
+++ server/routes/tiles.ts
@@ -1,20 +1,21 @@
 import express from 'express'
 import * as db from '../db/index.ts'
 import camelcaseKeys from 'camelcase-keys'
+import { SavedTileData } from 'model/tiles.ts'
 
 const router = express.Router()
 
 //Get home feed tiles [PUBLIC]
 router.get('/', async (req, res) => {
   try {
-    const data = await db.connection('tiles').where('is_private', 0).select()
-
     let tiles
+
+    const data = await db.getAllPublicTiles()
     data ? (tiles = camelcaseKeys(data)) : (tiles = [])
 
     res.json(tiles)
   } catch {
-    res.status(500)
+    res.sendStatus(500)
   }
 })
 
@@ -22,39 +23,62 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tileId = req.params.id
-    const data = await db
-      .connection('tiles')
-      .where('id', tileId)
-      .select()
-      .first()
-
     let tile
+
+    const data = await db.getTileById(Number(tileId))
     data ? (tile = camelcaseKeys(data)) : (tile = {})
 
     res.json(tile)
   } catch {
-    res.status(500)
+    res.sendStatus(500)
   }
 })
 
-//TODO Get all public tiles
-// router.post('/', async (req, res) => {
-//   try {
-//     const newTile = {
-//       title: 'Test tile',
-//       description: 'This is a new tile, created for testing purposes.',
-//       is_private: false,
-//     }
+//Get user saved tiles
+router.get('/saved/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId
+    let tiles
 
-//     const { data, error } = await supabase
-//       .from('tiles')
-//       .insert(newTile)
-//       .select()
+    const data = await db.getUserSavedTiles(Number(userId))
+    data ? (tiles = camelcaseKeys(data)) : (tiles = [])
 
-//     error ? console.log(error.message) : res.json(data)
-//   } catch {
-//     res.status(500)
-//   }
-// })
+    res.json(tiles)
+  } catch {
+    res.sendStatus(500)
+  }
+})
+
+// Save/Unsave a tile
+router.put('/saved', async (req, res) => {
+  try {
+    const saveRequest = req.body as SavedTileData
+
+    // Look for existing saved tile record
+    let existingRecord = await db.findSavedTileRecord(
+      Number(saveRequest.tileId),
+      Number(saveRequest.savedBy),
+    )
+
+    // If record exists, check the timestamp and update is_saved if needed
+    if (existingRecord) {
+      existingRecord = camelcaseKeys(existingRecord)
+      if (saveRequest.updatedAt > existingRecord.updatedAt) {
+        const updatedRecord = await db.updateSavedTileRecord(saveRequest)
+        res.status(204).json(updatedRecord)
+      } else {
+        res.sendStatus(204)
+      }
+    }
+    // Else create a new record
+    else {
+      const newRecord = await db.createSavedTileRecord(saveRequest)
+      res.status(201).json(newRecord)
+    }
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
 
 export default router
