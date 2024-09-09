5ea36cbccce3fd2a4da4df205bc61b896cf31dd1
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-09-09 14:21:16 +1200

    Use improved database models, and user table queries

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,6 +1,11 @@
 import knexFile from './knexfile.js'
 import knex from 'knex'
-import { SavedTileData, UserSavedTilesTable, TilesTable } from 'model/tiles.ts'
+import {
+  SavedTileData,
+  UserSavedTilesTable,
+  TilesTable,
+  Tile,
+} from 'model/tiles.ts'
 import { UsersTable } from 'model/users.ts'
 
 type Environment = 'production' | 'test' | 'development'
@@ -9,56 +14,54 @@ const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
 export const connection = knex(config)
 
-//Server Functions
+//Database Query Functions
 
-export function getAllPublicTiles() {
-  return connection('tiles').where(TilesTable.isPrivate, 0).select()
-}
-
-export function getAllPublicTilesWithSavedStatus(userId: number) {
-  return connection('tiles')
-    .leftJoin('user_saved_tiles', function () {
-      this.on(
-        `tiles.${TilesTable.id}`,
-        `user_saved_tiles.${UserSavedTilesTable.tileId}`,
-      ).andOn(
-        `user_saved_tiles.${UserSavedTilesTable.savedBy}`,
-        userId.toString(),
+export function getPublicTiles(savedBy: number | null) {
+  return connection(TilesTable.table)
+    .leftJoin(UserSavedTilesTable.table, function () {
+      this.on(TilesTable.id, UserSavedTilesTable.tileId).andOn(
+        UserSavedTilesTable.savedBy,
+        connection.raw('?', [savedBy]),
       )
     })
-    .where('tiles.is_private', 0)
-    .select('tiles.*', `user_saved_tiles.${UserSavedTilesTable.isSaved}`)
+    .where(TilesTable.isPrivate, 0)
+    .select(TilesTable.all, UserSavedTilesTable.isSaved)
 }
+// This query function will likely be the first to optimise;
+// Especally if adding some kind of ranked sorting.
+// look into indexing (tile_id, saved_by, isPrivate)
+// and paginating results (.limit and .offset)
 
-export function getTileById(tileId: number) {
-  return connection('tiles').where(TilesTable.id, tileId).select().first()
+export function getTileById(tileId: number, savedBy: number | null) {
+  return connection(TilesTable.table)
+    .leftJoin(UserSavedTilesTable.table, function () {
+      this.on(TilesTable.id, UserSavedTilesTable.tileId).andOn(
+        UserSavedTilesTable.savedBy,
+        connection.raw('?', [savedBy]),
+      )
+    })
+    .where(TilesTable.id, tileId)
+    .select(TilesTable.all, UserSavedTilesTable.isSaved)
+    .first()
 }
 
 export function getUserSavedTiles(userId: number) {
-  return connection('users')
-    .where(`users.${UsersTable.id}`, userId)
-    .join(
-      'user_saved_tiles',
-      `user_saved_tiles.${UserSavedTilesTable.savedBy}`,
-      `users.${UsersTable.id}`,
-    )
-    .join(
-      'tiles',
-      `tiles.${TilesTable.id}`,
-      `user_saved_tiles.${UserSavedTilesTable.tileId}`,
-    )
-    .select('tiles.*')
+  return connection(UsersTable.table)
+    .where(UsersTable.id, userId)
+    .join(UserSavedTilesTable.table, UserSavedTilesTable.savedBy, UsersTable.id)
+    .join(TilesTable.table, TilesTable.id, UserSavedTilesTable.tileId)
+    .select(TilesTable.all)
 }
 
 export function getSavedTileRecord(recordId: number) {
-  return connection('user_saved_tiles')
+  return connection(UserSavedTilesTable.table)
     .where(UserSavedTilesTable.id, recordId)
     .select()
     .first()
 }
 
 export function findSavedTileRecord(tileId: number, userId: number) {
-  return connection('user_saved_tiles')
+  return connection(UserSavedTilesTable.table)
     .where(UserSavedTilesTable.savedBy, userId)
     .where(UserSavedTilesTable.tileId, tileId)
     .select()
@@ -66,27 +69,40 @@ export function findSavedTileRecord(tileId: number, userId: number) {
 }
 
 export function updateSavedTileRecord(data: SavedTileData) {
-  return connection('user_saved_tiles')
+  return connection(UserSavedTilesTable.table)
     .where(UserSavedTilesTable.savedBy, data.savedBy)
     .where(UserSavedTilesTable.tileId, data.tileId)
     .update({
-      [UserSavedTilesTable.isSaved]: data.isSaved,
-      [UserSavedTilesTable.updatedAt]: data.updatedAt,
+      is_saved: data.isSaved,
+      updated_at: data.updatedAt,
     })
     .returning(UserSavedTilesTable.id)
 }
 
 export function createSavedTileRecord(data: SavedTileData) {
-  return connection('user_saved_tiles')
+  return connection(UserSavedTilesTable.table)
     .insert({
-      [UserSavedTilesTable.savedBy]: data.savedBy,
-      [UserSavedTilesTable.tileId]: data.tileId,
-      [UserSavedTilesTable.isSaved]: data.isSaved,
-      [UserSavedTilesTable.updatedAt]: data.updatedAt,
+      saved_by: data.savedBy,
+      tile_id: data.tileId,
+      is_saved: data.isSaved,
+      updated_at: data.updatedAt,
     })
     .returning(UserSavedTilesTable.id)
 }
 
 export function getAllSavedTileRecords() {
-  return connection('user_saved_tiles').select()
+  return connection(UserSavedTilesTable.table).select()
+}
+
+export function getUserById(userId: number) {
+  return connection(UsersTable.table)
+    .where(UsersTable.id, userId)
+    .select()
+    .first()
+}
+
+export function findUserName(username: string) {
+  return connection(UsersTable.table)
+    .where(UsersTable.username, username)
+    .select()
 }
