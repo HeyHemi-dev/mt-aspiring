c5a11110be7bfc9800be44416a56dfd738956a57
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-09-09 17:17:07 +1200

    Show current users saved tiles

--- client/components/pages/UserProfile.tsx
+++ client/components/pages/UserProfile.tsx
@@ -1,7 +1,30 @@
+import { useQuery } from '@tanstack/react-query'
+import TileList from '../TileList'
+import * as api from '../../api/apiClient'
+import useCurrentUser from '@/hooks/useCurrentUser'
+
 function UserProfile() {
+  const userQuery = useCurrentUser()
+
+  const { data, isPending, isError, isSuccess } = useQuery({
+    queryKey: ['tiles', userQuery.data?.id],
+    queryFn: () => api.getSavedTiles(userQuery.data?.id),
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    enabled: !!userQuery.data?.id,
+  })
+
+  let outlet = <></>
+  isPending && (outlet = <p>Loading...</p>)
+  isError && (outlet = <p>Error Loading Tiles</p>)
+  isSuccess && (outlet = <TileList tiles={data} />)
+
+  // console.log(data)
+
   return (
     <>
-      <div>User Profile</div>
+      <section className="site-padding section-padding">{outlet}</section>
     </>
   )
 }
