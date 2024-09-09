7a69c998fef64df8c69e8a7ee6256f14ffce88e6
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-09-09 14:23:54 +1200

    Make images take up full height of grid cells

--- client/components/TileListItem.tsx
+++ client/components/TileListItem.tsx
@@ -5,18 +5,16 @@ import { Link } from 'react-router-dom'
 function TileListItem({ tile }: { tile: Tile }) {
   const tileLink = `tiles/${tile.id}`
   return (
-    <Card className="tile-list-item grid grid-rows-[minmax(0,1fr)_auto] overflow-hidden">
-      <Link to={tileLink} className="flex">
+    <Card className="tile-list-item grid grid-rows-subgrid overflow-hidden">
+      <Link to={tileLink} className="col-span-full row-span-full flex">
         <img
           src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
           alt={tile.title}
           className="h-full w-full rounded-md object-cover"
         />
       </Link>
-      <CardContent className="p-2">
-        <Link to={tileLink}>
-          <CardTitle>{tile.title}</CardTitle>
-        </Link>
+      <CardContent className="pointer-events-none col-span-full row-span-full flex items-end p-2">
+        <CardTitle>{tile.title}</CardTitle>
       </CardContent>
     </Card>
   )
